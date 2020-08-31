9e88fb84c8adb80cc54e9437b3ad73e48e40d46e
LeviHeo <leviheo@gmail.com>
2020-08-31 13:37:10 +0800

    realtime test

--- src/js/app.js
+++ src/js/app.js
@@ -416,18 +416,6 @@ $(function(){
    * https://developers.google.com/explorer-help/guides/code_samples#javascript
    */
 
-    function authenticate() {
-        return gapi.auth2.getAuthInstance()
-            .signIn({scope: "https://www.googleapis.com/auth/analytics https://www.googleapis.com/auth/analytics.readonly"})
-            .then(function() { console.log("Sign-in successful"); },
-                function(err) { console.error("Error signing in", err); });
-    }
-    function loadClient() {
-        gapi.client.setApiKey("YOUR_API_KEY");
-        return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/analytics/v3/rest")
-            .then(function() { console.log("GAPI client loaded for API"); },
-                function(err) { console.error("Error loading GAPI client for API", err); });
-    }
     // Make sure the client is loaded and sign-in is complete before calling this method.
     function execute() {
         return gapi.client.analytics.data.realtime.get({
@@ -440,8 +428,9 @@ $(function(){
                 },
                 function(err) { console.error("Execute error", err); });
     }
+    
     gapi.load("client:auth2", function() {
-        gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
+        gapi.auth2.init({client_id: "leviheo"});
     });
 
     execute();
@@ -454,5 +443,5 @@ $(function(){
         success: function(res) {
             console.log(res);
         }
-    })
+    });
 });
